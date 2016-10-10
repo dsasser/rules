02f360ddcc152ae1519555679d3147af65a2b5ca
Daniel Sasser <daniel.sasser@@workday.com>
2016-10-10 09:59:54 -0700

    #2815967 - Lookup user roles before trying to execute UserRoleAdd action.

--- src/Plugin/RulesAction/UserRoleAdd.php
+++ src/Plugin/RulesAction/UserRoleAdd.php
@@ -5,6 +5,7 @@ namespace Drupal\rules\Plugin\RulesAction;
 use Drupal\rules\Core\RulesActionBase;
 use Drupal\rules\Exception\InvalidArgumentException;
 use Drupal\user\UserInterface;
+use Drupal\user\Entity\Role;
 
 /**
  * Provides a 'Add user role' action.
@@ -47,7 +48,24 @@ class UserRoleAdd extends RulesActionBase {
    * @throws \Drupal\rules\Exception\InvalidArgumentException
    */
   protected function doExecute(UserInterface $account, array $roles) {
+    $drupal_roles = Role::loadMultiple();
     foreach ($roles as $role) {
+      // If the role is not a role entity yet, load it if we can.
+      if (!$role instanceof Role) {
+        // Try to load the role by role id.
+        if ($r = Role::load($role)) {
+          $role = $r;
+        }
+        else {
+          // Try to load the role by role label.
+          foreach ($drupal_roles as $drupal_role) {
+            if ($drupal_role->get('label') == $role) {
+              $role = $drupal_role;
+              break;
+            }
+          }
+        }
+      }
       // Skip adding the role to the user if they already have it.
       if (!$account->hasRole($role->id())) {
         // If you try to add anonymous or authenticated role to user, Drupal
