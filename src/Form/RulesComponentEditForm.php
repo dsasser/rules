ac10aef59d696b354ec4ebb4d6fb363aa0940561
Daniel Sasser <daniel.sasser@workday.com>
2016-11-23 11:43:05 -0800

    Issue #2799361 by coderwan: Ensure component config entity is updated from component changes during save.

--- src/Form/RulesComponentEditForm.php
+++ src/Form/RulesComponentEditForm.php
@@ -72,6 +72,8 @@ class RulesComponentEditForm extends RulesComponentFormBase {
    */
   public function save(array $form, FormStateInterface $form_state) {
     $this->rulesUiHandler->getForm()->submitForm($form, $form_state);
+    $component = $this->rulesUiHandler->getComponent();
+    $this->entity->updateFromComponent($component);
 
     // Persist changes by saving the entity.
     parent::save($form, $form_state);
